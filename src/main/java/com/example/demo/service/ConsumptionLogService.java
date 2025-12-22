dd2eaa91e629fa7d9702ee6c547c4dd9e9749423
bala-amypo <bala@amypo.in>
2025-12-22 08:37:29 +0000

    User commit

--- src/main/java/com/example/demo/service/ConsumptionLogService.java
+++ src/main/java/com/example/demo/service/ConsumptionLogService.java
@@ -5,10 +5,7 @@ import com.example.demo.model.ConsumptionLog;
 import java.util.List;
 
 public interface ConsumptionLogService {
-
     ConsumptionLog logConsumption(Long stockRecordId, ConsumptionLog log);
-
     List<ConsumptionLog> getLogsByStockRecord(Long stockRecordId);
-
     ConsumptionLog getLog(Long id);
 }
--- src/main/java/com/example/demo/service/PredictionService.java
+++ src/main/java/com/example/demo/service/PredictionService.java
@@ -6,10 +6,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface PredictionService {
-
     LocalDate predictRestockDate(Long stockRecordId);
-
-    PredictionRule createRule(PredictionRule rule);
-
     List<PredictionRule> getAllRules();
+    PredictionRule createRule(PredictionRule rule);
 }
