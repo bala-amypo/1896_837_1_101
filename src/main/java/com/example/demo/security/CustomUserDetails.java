ab23466c6123653b63e58ab9daba737b55b5190d
bala-amypo <bala@amypo.in>
2025-12-26 05:24:34 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetails.java
+++ src/main/java/com/example/demo/security/CustomUserDetails.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.stream.Collectors;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetails implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
